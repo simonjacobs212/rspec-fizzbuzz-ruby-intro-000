bda49f2ac7560116e43c35ce70b42ce0c6dc0984
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-10-19 21:06:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
-  if int % 5 == 0
+  if int % 3 == 0 && int % 5 == 0
+    puts = "FizzBuzz"
+  elsif int % 5 == 0
     puts = "Buzz"
   elsif int % 3 == 0
     puts = "Fizz"
-  elsif int % 3 == 0 && int % 5 == 0
-      puts = "FizzBuzz"
   else
     puts int
   end
